2cd8f83cdeea335aa6c0e6b3cd90f36e6316d22a
emilyinure <114822181+emilyinure@users.noreply.github.com>
2025-10-30 02:03:41 -0400

    Forgot the next config

--- mc-exchange-frontend/shop-ui/next.config.ts
+++ mc-exchange-frontend/shop-ui/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "mc.nerothe.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
